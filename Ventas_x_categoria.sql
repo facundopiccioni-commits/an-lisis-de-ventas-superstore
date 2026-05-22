1583de5bcdaa0e126288daa5105cb8d7e2fec423
facundopiccioni-commits <facundopiccioni@gmail.com>
2026-05-22 13:32:27 -0300

    Delete Ventas_x_categoria.sql

--- Ventas_x_categoria.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    category,
-    COUNT(order_id)         AS total_ordenes,
-    ROUND(SUM(sales), 2)    AS ventas_totales,
-    ROUND(SUM(profit), 2)   AS ganancia_total,
-    ROUND(AVG(discount), 2) AS descuento_promedio
-FROM orders
-GROUP BY category
-ORDER BY ventas_totales DESC;
