49228a14786795dab6cbff6b1c9125726eefc853
facundopiccioni-commits <facundopiccioni@gmail.com>
2026-05-22 13:33:11 -0300

    Delete ventas_x_subcategoria.sql

--- ventas_x_subcategoria.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    category,
-    sub_category,
-    ROUND(SUM(sales), 2)                                    AS ventas_totales,
-    ROUND(SUM(profit), 2)                                   AS ganancia_total,
-    ROUND(SUM(profit) / NULLIF(SUM(sales), 0) * 100, 2)    AS margen_pct
-FROM orders
-GROUP BY category, sub_category
-ORDER BY margen_pct ASC;
