8a9c0caa19cdda79c7d45aaa4051cac5b203fca9
facundopiccioni-commits <facundopiccioni@gmail.com>
2026-05-22 13:32:55 -0300

    Delete ventas_x_año.sql

--- ventas_x_año.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    EXTRACT(YEAR FROM order_date)   AS anio,
-    ROUND(SUM(sales), 2)            AS ventas_totales,
-    ROUND(SUM(profit), 2)           AS ganancia_total,
-    COUNT(DISTINCT order_id)        AS total_ordenes
-FROM orders
-GROUP BY anio
-ORDER BY anio ASC;
