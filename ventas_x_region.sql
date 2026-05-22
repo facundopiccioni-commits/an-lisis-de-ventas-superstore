4885358193cc544d9fea9e5bfa6e0cc65a387cca
facundopiccioni-commits <facundopiccioni@gmail.com>
2026-05-22 13:33:04 -0300

    Delete ventas_x_region.sql

--- ventas_x_region.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    region,
-    ROUND(SUM(sales), 2)                                    AS ventas_totales,
-    ROUND(SUM(profit), 2)                                   AS ganancia_total,
-    ROUND(SUM(profit) / NULLIF(SUM(sales), 0) * 100, 2)    AS margen_pct,
-    COUNT(DISTINCT customer_id)                             AS clientes_unicos
-FROM orders
-GROUP BY region
-ORDER BY ventas_totales DESC;
